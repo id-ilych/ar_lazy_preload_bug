a31cb73ed8faba5cc4dacacadb7a183700deb031
Ilya Denisov <id.ilych@gmail.com>
2024-05-23 21:54:35 +0300

    add similar passing test

--- test/models/alice_test.rb
+++ test/models/alice_test.rb
@@ -1,16 +1,35 @@
 require "test_helper"
 
 class AliceTest < ActiveSupport::TestCase
-  test "the truth" do
+  test "one" do
     alice = Alice.create(alice: 1)
     bob = Bob.create(bob: 2, alice:)
     charlie = Charlie.create(charlie: 3, bob:)
 
     record = Alice.preload_associations_lazily.find(alice.id)
     assert record.lazy_preload_context.present?, "lazy context is missing for alice"
+
     assert_equal loaded_associations(record), [], "some associations are preloaded"
     assert_equal record.charlie.charlie, 3, "unexpected charlie value"
     assert_equal loaded_associations(record), %w[bob charlie], "unexpected associations preloaded"
+
+    assert record.charlie.lazy_preload_context.present?, "lazy context is missing for charlie"
+    assert record.bob.lazy_preload_context.present?, "lazy context is missing for bob"
+  end
+
+  test "two" do
+    alice = Alice.create(alice: 1)
+    bob = Bob.create(bob: 2, alice:)
+    charlie = Charlie.create(charlie: 3, bob:)
+
+    record = Alice.preload_associations_lazily.find(alice.id)
+    assert record.lazy_preload_context.present?, "lazy context is missing for alice"
+
+    assert_equal loaded_associations(record), [], "some associations are preloaded"
+    assert_equal record.bob.bob, 2, "unexpected bob value"
+    assert_equal record.charlie.charlie, 3, "unexpected charlie value"
+    assert_equal loaded_associations(record), %w[bob charlie], "unexpected associations preloaded"
+
     assert record.charlie.lazy_preload_context.present?, "lazy context is missing for charlie"
     assert record.bob.lazy_preload_context.present?, "lazy context is missing for bob"
   end
